5280a9eb8adc6f76d1f4aa2bb659a21eeb685117
Logan Omelchuk <72324766+hamsandvich@users.noreply.github.com>
2022-12-04 18:40:49 -0700

    Day1

---
+++ Day 1/CalorieCountingPart1.py
@@ -0,0 +1,29 @@
+## 
+
+with open ("z:/Logan's Shit/Code/Advent of Code 2022/Advent-Of-Code-2022/Day 1/input.txt",'r') as f:
+    lines = f.readlines()
+
+print(lines)
+elflist = []
+calories = 0
+elf = "elf #"
+elfnum = 1
+for i in range(len(lines)):
+    line = lines[i]
+    line = line.split('\n')
+
+    if len(line[0]) < 1:
+        print(elf + str(elfnum) + " has " + str(calories) + " calories")
+        elflist.append(calories)
+        calories = 0
+        elfnum += 1
+    else:
+        calories += int(line[0])
+print(elf + str(elfnum) + " has " + str(calories) + " calories")
+elflist.append(calories)
+max = 0
+for i in range(len(elflist)):
+    if int(elflist[i]) > max:
+        max = int(elflist[i])
+        index = i
+print("Elf #" + str(index + 1) + " has the most calories with " + str(max) + " calories")
---
+++ Day 1/CalorieCountingPart2.py
@@ -0,0 +1,25 @@
+## 
+
+with open ("z:/Logan's Shit/Code/Advent of Code 2022/Advent-Of-Code-2022/Day 1/input.txt",'r') as f:
+    lines = f.readlines()
+
+print(lines)
+elflist = []
+calories = 0
+elf = "elf #"
+elfnum = 1
+for i in range(len(lines)):
+    line = lines[i]
+    line = line.split('\n')
+
+    if len(line[0]) < 1:
+        print(elf + str(elfnum) + " has " + str(calories) + " calories")
+        elflist.append(calories)
+        calories = 0
+        elfnum += 1
+    else:
+        calories += int(line[0])
+print(elf + str(elfnum) + " has " + str(calories) + " calories")
+elflist.append(calories)
+elflist.sort(reverse=True)
+print(elflist[0]+elflist[1]+elflist[2])
